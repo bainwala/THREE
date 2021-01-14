56b07b9f8341c74f227bf3f0b9e7c6b169c12060
Himanshu Bainwala <hbainwala@gmail.com>
2021-01-13 22:22:39 -0800

    New

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-        var renderer = new THREE.WebGLRenderer();
+        var planet;
+        var renderer = new THREE.WebGLRenderer({alpha:true});
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
 
@@ -15,30 +15,36 @@ var scene = new THREE.Scene();
 
         //Create the Shape
 
+        const ambient = new THREE.AmbientLight(0x404040, 3);
+        scene.add(ambient);
+
+        var loader = new THREE.GLTFLoader();
+
+        loader.load("/jupiter/scene.gltf", function (gltf) { 
+            scene.add(gltf.scene);
+            planet = gltf.scene.children[0];
+            animate();
+         })
+
         var geometry = new THREE.BoxGeometry(1,1,1);
 
         var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: false});
 
         var cube = new THREE.Mesh(geometry, material)
 
-        scene.add(cube);
+        //scene.add(cube);
 
-        camera.position.z = 3;
+        camera.position.z = 6;
 
         var update = function() {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.05;
+            planet.rotation.z += 0.001;
+            planet.rotation.y += 0.0005;
         };
 
-        var render = function() {
+        var animate = function() {
+            requestAnimationFrame(animate);
             renderer.render(scene, camera);
+            update()
         }
 
-        var GameLoop = function() {
-            requestAnimationFrame(GameLoop);
-
-            update();
-            render();
-        };
-
-        GameLoop();
+        
